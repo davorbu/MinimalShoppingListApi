eea3887ee0c25359feae0d6093484545fdce8f7f
Davor Burmudžija <davorburmudzija@gmail.com>
2022-05-24 22:26:28 +0200

    Add project files.

---
+++ MinimalShoppingListApi/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalShoppingListApi
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Grocery> Groceries => Set<Grocery>();
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
